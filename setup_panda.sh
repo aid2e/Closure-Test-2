21c6c06d513f805f0291ef126f2617246881ab78
Wen Guan <wguan.icedew@gmail.com>
2024-06-16 09:51:00 +0200

    update env to use new doma panda in k8s

--- setup_panda.sh
+++ setup_panda.sh
@@ -1,6 +1,6 @@
 export PANDA_AUTH=oidc
-export PANDA_URL_SSL=https://pandaserver-doma.cern.ch:25443/server/panda
-export PANDA_URL=http://pandaserver-doma.cern.ch:25080/server/panda
+export PANDA_URL_SSL=https://pandaserver-doma.cern.ch/server/panda
+export PANDA_URL=https://pandaserver-doma.cern.ch/server/panda
 export PANDAMON_URL=https://panda-doma.cern.ch
 # export PANDA_AUTH_VO=panda_dev
 export PANDA_AUTH_VO=EIC
@@ -11,7 +11,7 @@ export PANDACACHE_URL=$PANDA_URL_SSL
 export PANDA_CONFIG_ROOT=~/.panda/
 
 # doma
-export IDDS_HOST=https://aipanda105.cern.ch:443/idds
+# export IDDS_HOST=https://aipanda105.cern.ch:443/idds
 
 export IDDS_LOG_LEVEL=debug
 
